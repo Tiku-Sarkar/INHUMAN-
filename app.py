cf756b95d8f17fe109398e88e971986c1ffdeb19
spiddy <tikusarkarwork@gmail.com>
2026-04-23 00:24:01 +0530

    Add the signin page

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, session, request
 
 app = Flask(__name__)
-
+app.secret_key = "your_secret_key"
 
 @app.route("/")
 def home():
@@ -20,15 +20,27 @@ def contact():
 
 @app.route("/login")
 def login():
+    session['user'] = "test_user"
     return "please login first"
 
 is_logged = False
 @app.route("/buy/<products>")
 def buy(products):
-    if not is_logged:
+    if 'user' not in  session:
         return redirect("/login")
     
     return f"you bought this {products}"
+
+
+@app.route("/signin", methods = ["POST","GET"])
+def signup():
+    if request.method == "POST":
+        name = request.form["name"]
+        email = request.form["email"]
+        password = request.form["password"]
+        return f"received {name} {email}"
+    
+    return render_template("signin.html")
 
 
 if __name__ == "__main__":
